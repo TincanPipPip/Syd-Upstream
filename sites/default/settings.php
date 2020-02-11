e576bae4e0405c3e1e2ce3c4c3bae5987f4dab8e
Rob Varkonyi <robert.varkonyi@gmail.com>
2020-02-11 13:18:01 +0100

    spekky tweaks

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -23,3 +23,14 @@ $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
 }
+$databases['default']['default'] = array (
+  'database' => 'syd_upstream',
+  'username' => 'root',
+  'password' => 'root',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+$settings['hash_salt'] = 'yXadRE77Va-G6dxhd2kPYapAvbnSvTF6hO4oXiOEynI';
