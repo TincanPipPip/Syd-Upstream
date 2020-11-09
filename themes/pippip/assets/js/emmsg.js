09ca6c77c6cdbbe9a51cae93f23cbeaa1714e9a3
Harry Edwards <harryedwards.nc@gmail.com>
2020-11-09 10:55:47 +0000

    Pippip updates

--- themes/pippip/assets/js/emmsg.js
+++ themes/pippip/assets/js/emmsg.js
@@ -1,16 +1,16 @@
 import Cookies from 'js-cookie';
 
-const emergencyMessage = document.getElementById('emmsg');
+const emergencyMessage = document.querySelector('.m-emmsg');
 
 if (emergencyMessage != null) {
-  if (!Cookies.get('hideemmsg')) {
+  if (!Cookies.get('apacHideEmmsg')) {
     emergencyMessage.setAttribute('data-state', 'visible');
   }
 
-  const btn = document.getElementById('emmsg-close');
+  const btn = document.querySelector('.m-emmsg__close');
 
   btn.addEventListener('click', function(e) {
-    Cookies.set('hideemmsg', 'true', { expires: 30 });
+    Cookies.set('apacHideEmmsg', 'true', { expires: 30 });
     emergencyMessage.setAttribute('data-state', 'hidden');
 
     e.preventDefault();
